cb9af82e8f70792f91022c23fff09b8d24c92b61
Zmar <159122092+SmetekTchur@users.noreply.github.com>
2024-11-20 11:34:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,3 +9,17 @@ print("Pole:", array)
 
 array1 = sorted(array)
 print("Jednoduchý sort:", array1)
+
+
+array3 = [10,54,575,45,4752,7,547862,8325785,47,42,47,12,4,4,2,4,2,1,4,5,54,654,1,35,465,43,51,6846,457]
+
+def bubble_sort ():
+    n = len(array3)
+    for i in range(n-1):
+        for j in range(0, n-i-1):
+            if array3[j] > array3[j+1]:
+                array3[j+1], array3[j] = array3[j], array3[j+1]
+                print(array3)
+    return array3
+
+print(bubble_sort())
